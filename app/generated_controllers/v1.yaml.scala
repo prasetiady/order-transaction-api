4dc64525475890b92c72aab9494ce635f7049e0e
prasetiady <dedy.berastagi@gmail.com>
2016-12-02 13:58:12 +0700

    remove dead code

--- app/generated_controllers/v1.yaml.scala
+++ app/generated_controllers/v1.yaml.scala
@@ -26,205 +26,71 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 package v1.yaml {
 
-    class Customer @Inject() (lifecycle: ApplicationLifecycle, config: ConfigurationProvider) extends CustomerBase {
-        // ----- Start of unmanaged code area for constructor Customer
+    class CustomerHandler @Inject() (lifecycle: ApplicationLifecycle, config: ConfigurationProvider) extends CustomerHandlerBase {
+        // ----- Start of unmanaged code area for constructor CustomerHandler
 
-        // ----- End of unmanaged code area for constructor Customer
-        val ordersGet = ordersGetAction { (customer_id: String) =>  
-            // ----- Start of unmanaged code area for action  Customer.ordersGet
+        // ----- End of unmanaged code area for constructor CustomerHandler
+        val ordersGet = ordersGetAction { (customerId: String) =>
+            // ----- Start of unmanaged code area for action  CustomerHandler.ordersGet
             NotImplementedYet
-            // ----- End of unmanaged code area for action  Customer.ordersGet
+            // ----- End of unmanaged code area for action  CustomerHandler.ordersGet
         }
-        val cartGet = cartGetAction { (customer_id: String) =>  
-            // ----- Start of unmanaged code area for action  Customer.cartGet
+        val cartGet = cartGetAction { (customerId: String) =>
+            // ----- Start of unmanaged code area for action  CustomerHandler.cartGet
             NotImplementedYet
-            // ----- End of unmanaged code area for action  Customer.cartGet
+            // ----- End of unmanaged code area for action  CustomerHandler.cartGet
         }
-    
-     // Dead code for absent methodItems.itemsGet
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemsGet
-            ItemsGet200(itemRepo.getAll())
-            // ----- End of unmanaged code area for action  Items.itemsGet
-     */
-
-    
-     // Dead code for absent methodItems.itemsPost
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemsPost
-            val f: Future[Item] = Future {
-              val id = Await.result(itemRepo.create(item), Duration.Inf)
-              val newItem: Item = item
-              newItem.copy(id = id)
-            }
-            ItemsPost200(f)
-            // ----- End of unmanaged code area for action  Items.itemsPost
-     */
-
-    
-     // Dead code for absent methodItems.itemGet
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemGet
-            val f: Future[Item] = Future {
-              Await.result(itemRepo.getById(id), Duration.Inf) match {
-                case Some(item) => item
-                case None => throw new Exception("Not found item with id: " + id)
-              }
-            }
-            ItemGet200(f)
-            // ----- End of unmanaged code area for action  Items.itemGet
-     */
-
-    
-     // Dead code for absent methodItems.itemPut
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemPut
-            val tempItem: Item = item
-            val newItem = tempItem.copy(id = id)
-            val f: Future[Item] = Future {
-              Await.result(itemRepo.update(newItem), Duration.Inf)
-              newItem
-            }
-            ItemPut200(f)
-            // ----- End of unmanaged code area for action  Items.itemPut
-     */
-
-    
-     // Dead code for absent methodOrder.verifiedPut
-     /*
-            // ----- Start of unmanaged code area for action  Order.verifiedPut
-            NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.verifiedPut
-     */
-
-    
-     // Dead code for absent methodItems.itemDelete
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemDelete
-            Await.result(itemRepo.delete(id), Duration.Inf)
-            ItemDelete200()
-            // ----- End of unmanaged code area for action  Items.itemDelete
-     */
-
-    
     }
 }
 package v1.yaml {
 
-    class Order @Inject() (lifecycle: ApplicationLifecycle, config: ConfigurationProvider) extends OrderBase {
-        // ----- Start of unmanaged code area for constructor Order
+    class OrderHandler @Inject() (lifecycle: ApplicationLifecycle, config: ConfigurationProvider) extends OrderHandlerBase {
+        // ----- Start of unmanaged code area for constructor OrderHandler
 
-        // ----- End of unmanaged code area for constructor Order
-        val paymentProffPost = paymentProffPostAction { input: (Int, String) =>
-            val (order_id, payment_proff) = input
-            // ----- Start of unmanaged code area for action  Order.paymentProffPost
-            NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.paymentProffPost
-        }
-        val shipPut = shipPutAction { (order_id: Int) =>  
-            // ----- Start of unmanaged code area for action  Order.shipPut
-            NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.shipPut
-        }
-        val orderGet = orderGetAction { (order_id: Int) =>  
-            // ----- Start of unmanaged code area for action  Order.orderGet
-            NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.orderGet
-        }
-        val verifyPut = verifyPutAction { (order_id: Int) =>  
-            // ----- Start of unmanaged code area for action  Order.verifyPut
-            NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.verifyPut
-        }
-        val cancelPut = cancelPutAction { (order_id: Int) =>  
-            // ----- Start of unmanaged code area for action  Order.cancelPut
-            NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.cancelPut
-        }
+        // ----- End of unmanaged code area for constructor OrderHandler
         val couponPost = couponPostAction { input: (Int, String) =>
-            val (order_id, coupon_code) = input
-            // ----- Start of unmanaged code area for action  Order.couponPost
+            val (orderId, coupon_code) = input
+            // ----- Start of unmanaged code area for action  OrderHandler.couponPost
             NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.couponPost
+            // ----- End of unmanaged code area for action  OrderHandler.couponPost
         }
-        val lineItemPost = lineItemPostAction { input: (Int, Int) =>
-            val (order_id, product_id) = input
-            // ----- Start of unmanaged code area for action  Order.lineItemPost
+        val orderGet = orderGetAction { (orderId: Int) =>
+            // ----- Start of unmanaged code area for action  OrderHandler.orderGet
             NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.lineItemPost
+            // ----- End of unmanaged code area for action  OrderHandler.orderGet
+        }
+        val shipPut = shipPutAction { (orderId: Int) =>
+            // ----- Start of unmanaged code area for action  OrderHandler.shipPut
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  OrderHandler.shipPut
+        }
+        val verifyPut = verifyPutAction { (orderId: Int) =>
+            // ----- Start of unmanaged code area for action  OrderHandler.verifyPut
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  OrderHandler.verifyPut
+        }
+        val paymentProffPost = paymentProffPostAction { input: (Int, String) =>
+            val (orderId, payment_proff) = input
+            // ----- Start of unmanaged code area for action  OrderHandler.paymentProffPost
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  OrderHandler.paymentProffPost
+        }
+        val cancelPut = cancelPutAction { (orderId: Int) =>
+            // ----- Start of unmanaged code area for action  OrderHandler.cancelPut
+            NotImplementedYet
+            // ----- End of unmanaged code area for action  OrderHandler.cancelPut
         }
         val checkoutPut = checkoutPutAction { input: (Int, ShippingAddress) =>
-            val (order_id, shipping_address) = input
-            // ----- Start of unmanaged code area for action  Order.checkoutPut
+            val (orderId, shippingAddress) = input
+            // ----- Start of unmanaged code area for action  OrderHandler.checkoutPut
             NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.checkoutPut
+            // ----- End of unmanaged code area for action  OrderHandler.checkoutPut
         }
-    
-     // Dead code for absent methodItems.itemsGet
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemsGet
-            ItemsGet200(itemRepo.getAll())
-            // ----- End of unmanaged code area for action  Items.itemsGet
-     */
-
-    
-     // Dead code for absent methodItems.itemsPost
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemsPost
-            val f: Future[Item] = Future {
-              val id = Await.result(itemRepo.create(item), Duration.Inf)
-              val newItem: Item = item
-              newItem.copy(id = id)
-            }
-            ItemsPost200(f)
-            // ----- End of unmanaged code area for action  Items.itemsPost
-     */
-
-    
-     // Dead code for absent methodItems.itemGet
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemGet
-            val f: Future[Item] = Future {
-              Await.result(itemRepo.getById(id), Duration.Inf) match {
-                case Some(item) => item
-                case None => throw new Exception("Not found item with id: " + id)
-              }
-            }
-            ItemGet200(f)
-            // ----- End of unmanaged code area for action  Items.itemGet
-     */
-
-    
-     // Dead code for absent methodItems.itemPut
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemPut
-            val tempItem: Item = item
-            val newItem = tempItem.copy(id = id)
-            val f: Future[Item] = Future {
-              Await.result(itemRepo.update(newItem), Duration.Inf)
-              newItem
-            }
-            ItemPut200(f)
-            // ----- End of unmanaged code area for action  Items.itemPut
-     */
-
-    
-     // Dead code for absent methodOrder.verifiedPut
-     /*
-            // ----- Start of unmanaged code area for action  Order.verifiedPut
+        val lineItemPost = lineItemPostAction { input: (Int, Int) =>
+            val (orderId, product_id) = input
+            // ----- Start of unmanaged code area for action  OrderHandler.lineItemPost
             NotImplementedYet
-            // ----- End of unmanaged code area for action  Order.verifiedPut
-     */
-
-    
-     // Dead code for absent methodItems.itemDelete
-     /*
-            // ----- Start of unmanaged code area for action  Items.itemDelete
-            Await.result(itemRepo.delete(id), Duration.Inf)
-            ItemDelete200()
-            // ----- End of unmanaged code area for action  Items.itemDelete
-     */
-
-    
+            // ----- End of unmanaged code area for action  OrderHandler.lineItemPost
+        }
     }
 }
